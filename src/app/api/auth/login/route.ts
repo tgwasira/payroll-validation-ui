14c834ca67954d28a63f1b4f5bcbd37213bb3718
Thomas Gwasira <tomgwasira@gmail.com>
2026-03-31 21:17:23 +0200

    Added typescript ignore.

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
--- src/app/api/proxy/[...path]/route.ts
+++ src/app/api/proxy/[...path]/route.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
